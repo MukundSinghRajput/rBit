7eda0ab85d5a15cd56ed579934cd42c2e28c5220
MukundSinghRajput <mukundsrajput@gmail.com>
2025-10-07 11:56:59 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use clap::Parser;
+
+/// A BitTorrent client written in rust.
+#[derive(Parser, Debug)]
+#[command(author = "Mukund", version, about, long_about = None)]
+struct Args {
+    /// Path to the .torrent or .magnet file
+    #[arg(short, long)]
+    source: String,
+
+    /// Directory to save the downloaded files too
+    #[arg(short, long, default_value = ".")]
+    output: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    println!("Torrent file {}", args.source);
+    println!("Output directory: {}", args.output)
+}
